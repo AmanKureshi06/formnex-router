cd7948f64fbf5a4ebf28e231607065350aa98c06
AmanKureshi06 <amankureshi060906@gmail.com>
2025-05-06 15:17:35 +0530

    Update deployment

--- app/endpoints/[id]/leads/page.tsx
+++ app/endpoints/[id]/leads/page.tsx
@@ -29,20 +29,22 @@ const pageData = {
   description: "All collected leads for this endpoint",
 };
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const leadsData = await getLeadsByEndpoint({
-    id: params.id,
-  });
+export default async function Page(
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const leadsData = await getLeadsByEndpoint({ id });
   const { data, serverError } = leadsData || {};
   if (!data || serverError) notFound();
   const { leadData: leads, schema } = data;
 
   return (
     <>
-      <Breadcrumbs leadId={params?.id} />
+      <Breadcrumbs leadId={id} />
       <PageWrapper>
         <Header title={pageData?.title}>{pageData?.description}</Header>
-        <ExportCSV id={params.id} leads={leads} schema={schema} />
+        <ExportCSV id={id} leads={leads} schema={schema} />
         <Table className="not-prose">
           <TableHeader>
             <TableRow className="bg-secondary hover:bg-secondary">
